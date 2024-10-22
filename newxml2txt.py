d9f9b69e325e644663f71c9610b5cf26bd500193
chelsea <clee1913@gmail.com>
2024-10-22 06:57:54 -0700

    formatting not working for diary 55, returned to original non-formatted method

--- newxml2txt.py
+++ newxml2txt.py
@@ -17,38 +17,42 @@ def xml_to_txt(xml_file, txt_file, initial_indent_level, initial_spacing_level):
         file.write(format_text(initial_indent_level, initial_spacing_level, ''))
         # Recursively extract text from XML tree
         def extract_text(element, indent_level=10, spacing_level=1):
+            if element.text:
+                 file.write(element.text.strip() + '\n')
+            
             # For specific elements, write the corresponding text
-            if element.tag in ['title', 'list', 'revisionDesc']:
-                if element.text:
-                    formatted_text = format_text(indent_level, spacing_level, element.text)
-                    file.write(formatted_text)
-            if element.tag in ['p', 'head']:
-                if element.text:
-                    formatted_text = format_text(indent_level+2, spacing_level, element.text)
-                    file.write(formatted_text)
-            if element.tag in ['author', 'date']:
-                if element.text:
-                    formatted_text = format_text(indent_level, 2, element.text)
-                    file.write(formatted_text)
-            if element.tag in ['name']:
-                if element.text:
-                    formatted_text = format_text(indent_level+2, 2, element.text)
-                    file.write(formatted_text)
-            if element.tag in ['resp']:
-                if element.text:
-                    formatted_text = format_text(indent_level+2, 3, element.text)
-                    file.write(formatted_text)
-            if element.tag in ['distributor', 'idno', 'bibl', 'projectDesc']:
-                if element.text:
-                    formatted_text = format_text(indent_level, 3, element.text)
-                    file.write(formatted_text)
-            if element.tag in ['availability']:
-                if element.text:
-                    formatted_text = format_text(indent_level+2, 2, element.text)
-                    file.write(formatted_text)
+            # if element.tag in ['title', 'list', 'revisionDesc', 'div']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level, spacing_level, element.text)
+            #         file.write(formatted_text)
+            # if element.tag in ['p', 'head']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level+2, spacing_level, element.text)
+            #         file.write(formatted_text)
+            # if element.tag in ['author', 'date']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level, 2, element.text)
+            #         file.write(formatted_text)
+            # if element.tag in ['name']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level+2, 2, element.text)
+            #         file.write(formatted_text)
+            # if element.tag in ['resp']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level+2, 3, element.text)
+            #         file.write(formatted_text)
+            # if element.tag in ['distributor', 'idno', 'bibl', 'projectDesc']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level, 3, element.text)
+            #         file.write(formatted_text)
+            # if element.tag in ['availability']:
+            #     if element.text:
+            #         formatted_text = format_text(indent_level+2, 2, element.text)
+            #         file.write(formatted_text)
+
             # Recursively extract text from children
             for child in element:
-                extract_text(child)
+                    extract_text(child)
 
             # Handle element tail text if present
             if element.tail:
@@ -56,7 +60,7 @@ def xml_to_txt(xml_file, txt_file, initial_indent_level, initial_spacing_level):
         # Start extracting from the root element
         extract_text(root)
 
-# Usage example, use pathname
-xml_file = '/Users/chelsea/SDP files/SDP/texts/d47pt1.xml' 
-txt_file = '/Users/chelsea/SDP files/SDP/texts/d47pt1me.txt'
+#Set desired xml,txt using pathname
+xml_file = '/Users/chelsea/SDP files/SDP/texts/diary55fix?.xml'
+txt_file = '/Users/chelsea/SDP files/SDP/texts/diary55.txt'
 xml_to_txt(xml_file, txt_file, initial_indent_level=10, initial_spacing_level=4)
